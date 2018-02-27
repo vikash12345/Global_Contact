777aba83ccdd667423b60212720bb44ea221572c
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-27 17:47:35 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,26 +2,38 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+// Global Directory for USA 
+//http://globalcontact.com/gc/directory/search.php?table=USBIZ&company=a&search=&search_sic=&page=1
+//My Scraper will scrape data A to Z and check whole pagination.
+$Alpha=array('1');
+//$Alpha=array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+for ($outterloop = 0; $outterloop < sizeof($Alpha); $outterloop++) 
+{
+	
+	$NewLink	=	'http://globalcontact.com/gc/directory/search.php?table=USBIZ&company='.$Alpha[$outterloop].'&search=&search_sic=&page=1';
+	$html		=	file_get_html($NewLink);
+	sleep(5);
+	if($html)
+	{
+		$link		=	$html->find("/html/body/center/table/tbody/tr[2]/td[2]/div/div[2]/table/tbody/tr/td[2]/div/div[4]/center/div/a[11]", 0);
+		$checker	=	 $link->href.'<br>';
+		$paginationlink	=	'http://www.globalcontact.com/gc/directory/'.$checker;
+		$pages= substr(strrchr($paginationlink, "="), 1);
+		for ($pagestart = 1; $pagestart <= $pages; $pagestart++)
+		{
+			$pagination = "http://globalcontact.com/gc/directory/search.php?table=USBIZ&company=$Alpha[$outterloop]&search=&search_sic=&page=$pagestart";
+			$mainpage	=	file_get_html($pagination);
+			if($mainpage)
+			{
+				echo $mainpage;
+			}
+			
+		}
+				
+	}
+	
+}
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
 ?>
